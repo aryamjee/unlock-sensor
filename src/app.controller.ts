600bf6469129673ff25b16593f7bfffa59396426
megha <aryamjee@gmail.com>
2020-07-23 01:33:25 +0530

    commit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, HttpCode, Body } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('unlock-sensor')
+  @HttpCode(200)
+  async unlockSensor(@Body() input: { gyroscope: string, accelerometer: string }): Promise<{}> {
+    return await this.appService.unlockSensor(input);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,8 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  async unlockSensor(input): Promise<{}> {
+    return input;
+  }
 }
