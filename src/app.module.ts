8ea144b69f349a481a36b4660158a1510185a9a7
megha <aryamjee@gmail.com>
2020-08-13 02:16:09 +0530

    commit for db connection setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseService } from './database/database.service';
+import { ConfigModule } from './config/config.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService,DatabaseService],
 })
 export class AppModule {}
---
+++ src/database/database.service.ts
@@ -0,0 +1,28 @@
+import { Injectable, OnModuleInit, HttpException } from '@nestjs/common';
+import { createConnection } from 'typeorm';
+import { ConfigModule } from 'src/config/config.module';
+import { ConfigService } from 'src/config/config.service';
+
+@Injectable()
+export class DatabaseService implements OnModuleInit {
+  constructor(private config: ConfigService) {}
+  async onModuleInit() {
+    await createConnection({
+      type: 'mysql',
+      host: this.config.get('DATABASE_HOST'),
+      port: Number(this.config.get('DATABASE_PORT')),
+      username: this.config.get('DATABASE_USER'),
+      password: this.config.get('DATABASE_PASSWORD'),
+      database: this.config.get('DATABASE_NAME'),
+      entities: [this.config.get('ENTITY_PATH')],
+      synchronize: false,
+      logging: false,
+      insecureAuth: true,
+    })
+      .then(done => console.log('Connection established'))
+      .catch(err => {
+        console.log(err);
+        throw new HttpException('Connection Error', 500);
+      });
+  }
+}
